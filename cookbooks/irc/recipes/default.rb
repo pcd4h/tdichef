3be376d8cd929aa862a96dbcad4348552b3d5a1b
Dirk Haertel <pcd4h@yahoo.com>
2016-05-19 11:31:05 +0200

    Adding TDI cookbooks

---
+++ cookbooks/irc/recipes/default.rb
@@ -0,0 +1,31 @@
+#
+# Cookbook Name:: irc
+# Recipe:: default
+#
+# Copyright 2016, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+user 'tdi' do
+  action :create
+  comment "Test Driven Infrastructure"
+  home "/home/tdi"
+  manage_home true
+  shell '/bin/bash'
+end
+
+package 'irssi' do
+  action :install
+end
+
+directory '/home/tdi/.irssi' do
+  owner 'tdi'
+  group 'tdi'
+end
+
+cookbook_file '/home/tdi/.irssi/config' do
+  source 'irssi-config'
+  owner 'tdi'
+  group 'tdi'
+end
